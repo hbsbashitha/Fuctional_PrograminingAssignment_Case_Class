319be2a308f38b39e1e602f5a4127fe9fe146f98
hbsbashitha <hbsbashitha@gmail.com>
2021-08-24 20:22:41 +0530

    completed

---
+++ q4.scala
@@ -0,0 +1,18 @@
+object  MyApp extends App{
+    val p1=Point(0,3)
+    val p2=Point(5,6)
+   
+    println(p1)
+    println(p1.invert())
+    
+    println(p2)
+    println(p2.invert())
+}
+
+case class Point(a:Int,b:Int){
+    def x:Int = a
+    def y:Int = b
+
+    def invert()=Point(y,x)
+    
+}
